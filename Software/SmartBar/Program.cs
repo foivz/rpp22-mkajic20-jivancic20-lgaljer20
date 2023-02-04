afb272c78726d671fcf0d295f39fab1324feb507
mkajic20 <mkajic20@foi.hr>
2023-02-04 12:51:16 +0100

    Dizajnirana forma za login. #3

--- Software/SmartBar/Program.cs
+++ Software/SmartBar/Program.cs
@@ -16,7 +16,7 @@ namespace SmartBar
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmLogin());
         }
     }
 }
