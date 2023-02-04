e4dae035348d5f1e93fa81502ea19d18a7410eb2
mkajic20 <mkajic20@foi.hr>
2023-02-04 13:15:32 +0100

    Kreiran repozitorij korisnika za dohvat korisnika. #3

---
+++ Software/DataAccessLayer/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using DataAccessLayer;
+using EntitiesLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartBar
+{
+    public class UserRepository
+    {
+        public User GetUser(string username)
+        {
+            using(var context = new DataBaseModel())
+            {
+                var result  = from k in context.Users
+                              where k.Username == username
+                              select k;
+
+                return result as User;
+            }
+        }
+    }
+}
