a91e87b357810d52cd6835dbde4073d622ebb1df
mkajic20 <mkajic20@foi.hr>
2023-02-10 15:06:25 +0100

    Kreirana forma za ispis računa. #4

---
+++ Software/SmartBar/FrmReceipts.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace SmartBar
+{
+    partial class FrmReceipts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvReceipts = new System.Windows.Forms.DataGridView();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReceipts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvReceipts
+            // 
+            this.dgvReceipts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvReceipts.Location = new System.Drawing.Point(12, 12);
+            this.dgvReceipts.Name = "dgvReceipts";
+            this.dgvReceipts.RowHeadersWidth = 51;
+            this.dgvReceipts.RowTemplate.Height = 24;
+            this.dgvReceipts.Size = new System.Drawing.Size(776, 392);
+            this.dgvReceipts.TabIndex = 0;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(692, 410);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(96, 28);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Storniraj";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(590, 410);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(96, 28);
+            this.btnShow.TabIndex = 2;
+            this.btnShow.Text = "Ispiši";
+            this.btnShow.UseVisualStyleBackColor = true;
+            // 
+            // FrmReceipts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.dgvReceipts);
+            this.Name = "FrmReceipts";
+            this.Text = "Računi";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReceipts)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvReceipts;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnShow;
+    }
+}
