fb8423889a738cd8ee4553814ebe23798d03cfc4
lgaljer20 <lgaljer20@foi.hr>
2023-02-11 13:13:42 +0100

    Dodan servis za poslovnu logiku za prikaz proizvoda, inventara pomoću grafa. #11

---
+++ Software/BusinessLogicLayer/Services/GraphProductService.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessLayer.Repositories;
+using EntitiesLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Services
+{
+    public class GraphProductService
+    {
+        public List<Product> GetAllGraph()
+        {
+            GraphProductRepository repo = new GraphProductRepository();
+
+            return repo.GetProductsGraph().ToList();
+        }
+    }
+}
