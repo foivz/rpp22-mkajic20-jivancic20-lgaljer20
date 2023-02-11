25fc7a768d7950d0d48989073643f518f3ee20d8
lgaljer20 <lgaljer20@foi.hr>
2023-02-11 13:13:14 +0100

    Dodan repository za dohvaćanje liste proizvoda za prikaz grafa. #11

---
+++ Software/DataAccessLayer/Repositories/GraphProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using EntitiesLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.PeerToPeer;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class GraphProductRepository
+    {
+        public List<Product> GetProductsGraph()
+        {
+            using (var context = new DataBaseModel())
+            {
+                try
+                {
+                    return context.Products.ToList();
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
