03aa2f046f1f4ce243656c0bdc66e658f4318174
mkajic20 <mkajic20@foi.hr>
2023-02-14 23:19:08 +0100

    Implementirano kreiranje pdf računa. #5

---
+++ Software/SmartBar/ReceiptReportForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace SmartBar
+{
+    partial class ReceiptReportForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource2 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.receiptBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.receiptListItemBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.receiptBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.receiptListItemBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer
+            // 
+            this.reportViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "dsReceipt";
+            reportDataSource1.Value = this.receiptBindingSource;
+            reportDataSource2.Name = "dsReceiptListItem";
+            reportDataSource2.Value = this.receiptListItemBindingSource;
+            this.reportViewer.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer.LocalReport.DataSources.Add(reportDataSource2);
+            this.reportViewer.LocalReport.ReportEmbeddedResource = "SmartBar.rptReceipt.rdlc";
+            this.reportViewer.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer.Name = "reportViewer";
+            this.reportViewer.ServerReport.BearerToken = null;
+            this.reportViewer.Size = new System.Drawing.Size(800, 450);
+            this.reportViewer.TabIndex = 0;
+            // 
+            // receiptBindingSource
+            // 
+            this.receiptBindingSource.DataSource = typeof(EntitiesLayer.Entities.Receipt);
+            // 
+            // receiptListItemBindingSource
+            // 
+            this.receiptListItemBindingSource.DataSource = typeof(EntitiesLayer.Entities.ReceiptListItem);
+            // 
+            // ReceiptReportForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.reportViewer);
+            this.Name = "ReceiptReportForm";
+            this.Text = "Račun";
+            this.Load += new System.EventHandler(this.ReceiptReportForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.receiptBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.receiptListItemBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer;
+        private System.Windows.Forms.BindingSource receiptBindingSource;
+        private System.Windows.Forms.BindingSource receiptListItemBindingSource;
+    }
+}
---
+++ Software/SmartBar/ReceiptReportForm.cs
@@ -0,0 +1,34 @@
+﻿using EntitiesLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmartBar
+{
+    public partial class ReceiptReportForm : Form
+    {
+        private Receipt receipt;
+        private List<ReceiptListItem> items;
+
+        public ReceiptReportForm(Receipt receipt, List<ReceiptListItem> items)
+        {
+            this.receipt = receipt;
+            this.items = items;
+            InitializeComponent();
+        }
+
+        private void ReceiptReportForm_Load(object sender, EventArgs e)
+        {
+            //this.reportViewer.RefreshReport();
+            this.reportViewer.RefreshReport();
+            receiptBindingSource.DataSource = receipt;
+            receiptListItemBindingSource.DataSource = items;
+        }
+    }
+}
